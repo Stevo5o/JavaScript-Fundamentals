74d47019fe5f05f7d7b572017a37024c913aa4d7
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-10-21 14:49:19 +0100

    Part 2 JS Fundamentals

---
+++ 08-The window Object and More Scope/script.js
@@ -0,0 +1,38 @@
+var names = ["Jeremy", "Jeffrey", "Jennifer", "Jackie" ];
+var noNames = [];
+var i;
+
+do {
+	var element = names.pop();
+	alert(element);
+} while (names.length > 0);
+
+/* while (names.length > 0) {
+	var element = names.pop();
+	alert(element);
+} */
+
+/* for (i = 0, len = noNames.length; i < len; i++) {
+	var name = noNames[i];
+	alert(name);
+} // if the condition is false for the first iteration it will not run */
+
+
+/* for (i = 0, len = names.length; i < len; i++) {
+	var name = names[i];
+	alert(name);
+} */
+
+/* for (var i = 0; i < names.length; i = i + 1) {
+	var name = names[i];
+	alert(name);
+}
+ */
+/* for (var i = 0; i < names.length; i = i + 1) {
+	var name = names[i];
+	alert(name);
+}
+ */
+/* for (var i = 0; i < 10; i = i + 1) {
+	alert(i);
+} */
