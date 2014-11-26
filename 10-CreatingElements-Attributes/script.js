9e3b772a4effa71e5b489683b3dee72b277d3ad0
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-26 11:41:48 +0000

    CreatingElements getElement vs querySelector

--- 10-CreatingElements-Attributes/script.js
+++ 10-CreatingElements-Attributes/script.js
@@ -1,13 +1,18 @@
 /* @ Stephen O'Connor */
+// live load list
   (function() {
      
-     var pElements = document.getElementsByTagName("p");
+     // var pElements = document.getElementsByTagName("p"); // returns live load list
+     
+     var pElements = document.querySelectorAll("p"); 
      
      alert(pElements.length); // 5
      
-     var zx = document.createElement("p");
-     
+     var zx = document.createElement("p");     
+         
      document.body.appendChild(zx);
+     
+     pElements = document.querySelectorAll("p");
      
      alert(pElements.length); // 6
      
