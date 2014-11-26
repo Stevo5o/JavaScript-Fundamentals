af5faf1e8f5429bfd10879501656882119180cca
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-26 11:33:40 +0000

    Add folder CreatingElements and js file.

--- 10-CreatingElements-Attributes/script.js
+++ 10-CreatingElements-Attributes/script.js
@@ -1,6 +1,14 @@
 /* @ Stephen O'Connor */
   (function() {
      
+     var pElements = document.getElementsByTagName("p");
      
+     alert(pElements.length); // 5
+     
+     var zx = document.createElement("p");
+     
+     document.body.appendChild(zx);
+     
+     alert(pElements.length); // 6
      
   }());
