929056939da4b784e2595a0e1aba865ecb651f64
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-28 12:52:38 +0000

    Timers & Animation setTimeout

---
+++ 12-Timers-Animation/script.js
@@ -0,0 +1,21 @@
+/* @ Stephen O'Connor */
+(function () {
+
+   var speed = 500,
+           i = 0,
+           doSomething = function () {
+              console.log("doSomething() executed " + (i + 1) + " times");
+              i = i + 1;
+
+              if (i < 10) {
+                 setTimeout(doSomething, speed);
+              }
+           };
+
+   var timer = setTimeout(doSomething, speed);
+   
+   // clearTimeout(timer);
+
+   // alert("hello");
+
+}());
