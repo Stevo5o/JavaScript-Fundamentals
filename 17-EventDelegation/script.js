35d8ae8f214abdbb68f1ead07af610684bffc0ea
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-29 20:09:27 +0000

    Refactoring Event Handling Delegation

--- 17-EventDelegation/script.js
+++ 17-EventDelegation/script.js
@@ -1,27 +1,20 @@
 /* @ Stephen O'Connor */
 (function () {
 
-// code is more flexible - event handler delegetion
-   eventUtility.addEvent(document, "click", function (event) {
+   var mouseHandler = function (event) {
       var target = eventUtility.getTarget(event),
-              tagName = target.tagName,
               classData = target.getAttribute("data-body-class");
-// if it has a value - true
+      // if it has a value - true
       if (classData) {
          eventUtility.preventDefault(event);
-         document.body.className = "";
+         if (event.type === "click") {
+            document.body.className = "";
+         } else {
+            document.body.className = classData;
+         }
       }
-   });
-   
-   // mouseover
-   eventUtility.addEvent(document, "mouseover", function (event) {
-      var target = eventUtility.getTarget(event),
-              tagName = target.tagName,
-              classData = target.getAttribute("data-body-class");
-// if it has a value - true
-      if (classData) {
-         eventUtility.preventDefault(event);
-         document.body.className = classData;
-      }
-   });
+   };
+
+   eventUtility.addEvent(document, "click", mouseHandler);
+   eventUtility.addEvent(document, "mouseover", mouseHandler);
 }());
