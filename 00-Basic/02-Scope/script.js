5426f607a4f9d57377725c6f53f6db6b9051c0c4
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-19 14:36:25 +0000

    scope with without function

--- 00-Basic/02-Scope/script.js
+++ 00-Basic/02-Scope/script.js
@@ -1,20 +1,40 @@
 var globalVar = "This is a global variable.";
 
-var globalFunction = function(paramOne) {
-	var localVar = "This is a local variable.";
-	
-	var localFunction = function() {
-		var superLocalVar = "Hello, World!";
-		var localVar = "override var";
-		alert(superLocalVar);		
-		alert(paramOne);
-		alert(globalVar);
-		alert(localVar);		
-		alert(foo);		
-	};
-	
-	localFunction();
-	alert(localVar);
+var globalFunction = function (paramOne) {
+   var localVar = "This is a local variable.";
+
+   var localFunction = function () {
+      var superLocalVar = "Hello, World!";
+      var localVar = "override var";
+      alert(superLocalVar);
+      alert(paramOne);
+      alert(globalVar);
+      alert(localVar);
+      alert(foo);
+   };
+
+   localFunction();
+   alert(localVar);
 };
 
 globalFunction(2);
+
+// scope in console
+var x = 1;
+console.log(x); // 1
+function bar() {
+   if (true) {
+      var x = 2;
+      console.log(x); // 2
+   }
+}
+bar();
+console.log(x); // 1
+
+var x = 1;
+console.log(x); // 1
+if (true) {
+  var x = 2;
+  console.log(x); // 2
+}
+console.log(x); // 2
