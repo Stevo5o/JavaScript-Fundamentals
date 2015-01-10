f4d0d5690a0f912d61e4dc1e821dd40ff7617b07
Stephen <Stephen@O-Connor-ToshL>
2015-01-10 12:53:11 +0000

    Basic object

--- 00-Basic/03-Objects/functions.js
+++ 00-Basic/03-Objects/functions.js
@@ -1,19 +1,30 @@
 /* @ Stephen O'Connor */
 // immediately invoked anonymous function
-  (function() {
-     
-      button = document.getElementById('btn');
-      input = document.getElementById('text_field');
+(function () {
 
-      button.addEventListener('click', function()
-      {
-      	console.log('Clicked');
-      });
+   button = document.getElementById('btn');
+   input = document.getElementById('text_field');
 
-      button.addEventListener('click', function()
-      {
-      	console.log('Other click listener');
-      });
-     
-  }());
+   button.addEventListener('click', function ()
+   {
+      console.log('Clicked');
+   });
+
+   button.addEventListener('click', function ()
+   {
+      console.log('Other click listener');
+   });
+
+   // basic object
+   stej = {
+      name: "SteJ",
+      kills: ["Javascript", "Java", "Skipping"],
+      "favorite color": "Purple"
+   };
+
+   stej["favorite color"] = "Black";
+
+   console.log(stej.name);
+   console.log(stej["favorite color"]);
+}());
 
