7c75f02b8ceb0938fef6aa31e881582614649e47
Stephen <Stephen@O-Connor-ToshL>
2015-01-11 13:24:01 +0000

    Basic object: access a key with a space. string " " add .join

--- 00-Basic/03-Objects/functions.js
+++ 00-Basic/03-Objects/functions.js
@@ -2,6 +2,7 @@
 // immediately invoked anonymous function
 (function () {
 
+   // basic DOM access
    button = document.getElementById('btn');
    input = document.getElementById('text_field');
 
@@ -15,16 +16,23 @@
       console.log('Other click listener');
    });
 
-   // basic object
+   // basic object. key value pairs
    stej = {
       name: "SteJ",
       kills: ["Javascript", "Java", "Skipping"],
-      "favorite color": "Purple"
+      "favorite color": "Purple",
+      greet: function()
+      {
+         console.log("Hello, I am SteJ");
+      }
    };
 
+   // access a key with a space. string " "
    stej["favorite color"] = "Black";
 
    console.log(stej.name);
    console.log(stej["favorite color"]);
+   console.log(stej["kills"].join(', '));
+   stej.greet();
 }());
 
