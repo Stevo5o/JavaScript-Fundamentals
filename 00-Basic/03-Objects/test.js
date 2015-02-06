78c2b12967fe065257d7f290e32342fcd20c85c4
Stephen <Stephen@O-Connor-ToshL>
2015-02-06 13:58:34 +0000

    testing JS

--- 00-Basic/03-Objects/test.js
+++ 00-Basic/03-Objects/test.js
@@ -14,7 +14,7 @@
       name: "name",
       age: 0
    };
-   
+
    console.log( person.name, person.age );
 
    person = function ( name, age )
@@ -27,6 +27,36 @@
    stej = new person( "SteJ", 38 );
    console.log( barry.name, barry.age + '\n' +
            stej.name, stej.age + '\n' );
+
+   var funcs = [ ];
+   var index = 0;
+
+   function createfunc( i ) {
+      return function () {
+         console.log( "My value: " + i );
+         index++;
+         
+      };
+   }
+
+   for ( var i = 0; i < 3; i++ ) {
+      funcs[i] = createfunc( i );
+   }
+
+   for ( var j = 0; j < 3; j++ ) {
+      funcs[j]();                        // and now let's run each one to see
+   }
+   
+   if ( index === 0)
+         {
+            console.log("Yes");
+         }
+         else {
+            console.log('No')
+         }
+
+
+
 }() );
 
   
