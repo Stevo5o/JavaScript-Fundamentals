2195c7ff86c94cbdd259ffe6afbc7b2a70b4c0f9
Stephen <Stephen@O-Connor-ToshL>
2015-02-07 15:13:15 +0000

    function prototype inheritance

--- 01-Advanced/04-FunctionsAsObjects/funcasobj.js
+++ 01-Advanced/04-FunctionsAsObjects/funcasobj.js
@@ -1,38 +1,58 @@
 /**
  * funcasobj.js Discription
  * @ Stephen O'Connor, 06-Feb-2015
- * 
- * Dependencies:
- * 
+ *
+ * Dependencies: node.js to run
+ *
  */
 
 // immediately invoked anonymous function
-( function () {
+(function() {
 
-   var calculate = function ()
-   {
-      Array.prototype.pop
-      var fn = argumants.pop();
-      return fn( x, y );
-   };
+	var obj = {
+		name: "obj object",
+		doSomthing: function(x, y) {
+			console.log(this.name + " " + x + " " + y );
+		}
+	};
 
-   var obj =
-           {
-              name: "obj object",
-              doSomthing: function ()
-              {
-                 console.log( this.name );
-              }
-           };
+	var foo = {
+		name: "foo object"
+	};
 
-   var foo =
-           {
-              name: "foo object"
-           };
+	var bar = {
+		name: "bar object"
+	};
 
-   var bar =
-           {
-              name : "bar object"
-           };
-obj.doSomthing.apply(bar);
-}() );
+	obj.doSomthing.call(foo, 4, 5);
+	obj.doSomthing.apply(bar, [4, 5]);
+
+	var calculate = function() {		
+		var fn = Array.prototype.pop.apply(arguments);
+		return fn.apply(null, arguments);
+	};
+
+	var sum = function() {
+		var total = 0;
+
+		for (var i = 0, len = arguments.length; i < len; i++) {
+			total = total + arguments[i];
+		}
+		return total;
+	};
+
+	var diff = function() {
+		var total = Array.prototype.shift.apply(arguments);
+
+		for (var i = 0, len = arguments.length; i < len; i++) {
+			total = total - arguments[i];
+		}
+		return total;
+	};
+
+	var sumResult = calculate(1, 2, 3, 4, 5, sum),
+			diffResult = calculate(1, 2, 3, 4, 5, diff);
+
+	console.log("sum: " + sumResult + "\n" + "diff: " + diffResult)
+
+}());
