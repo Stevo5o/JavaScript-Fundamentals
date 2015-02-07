a8599fdd7552a02dfb66b127a185723d7b6a7dec
Stephen <Stephen@O-Connor-ToshL>
2015-02-07 16:04:53 +0000

    Event keypress

---
+++ 01-Advanced/05-Events/script.js
@@ -0,0 +1,44 @@
+/**
+ * newjavascript.js Discription
+ * @ Stephen O'Connor, 07-Feb-2015
+ *
+ * Dependencies: eventUtility.js
+ *
+ */
+
+// immediately invoked anonymous function
+(function() {
+
+	var txtbox = document.getElementById("txtInput");
+
+	eventUtility.addEvent(txtbox, "keypress",
+		function(evt) {
+			var code = eventUtility.getCharCode(evt);
+
+			if ((code >= 65 && code <= 90) ||
+				(code >= 97 && code <= 122)) {
+
+			} else {
+				eventUtility.preventDefault(evt);
+			}
+
+			// alert(code);
+
+			// evt.keyCode
+
+			// A 65
+			// B 66
+			// Z 90
+
+			// a 97
+			// z 122
+
+			// 0 48
+			// 9 57
+		});
+
+	// keydown
+	// keyup
+	// keypress
+
+}());
