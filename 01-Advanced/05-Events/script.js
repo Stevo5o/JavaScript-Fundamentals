e5eec0f278c8caab4a1503f7b3f77cc1818a1bbf
Stephen <Stephen@O-Connor-ToshL>
2015-02-08 13:15:15 +0000

    Event keydown

--- 01-Advanced/05-Events/script.js
+++ 01-Advanced/05-Events/script.js
@@ -11,16 +11,25 @@
 
 	var txtbox = document.getElementById("txtInput");
 
-	eventUtility.addEvent(txtbox, "keypress",
+	// allow only letters
+	eventUtility.addEvent(document, "keydown",
 		function(evt) {
-			var code = eventUtility.getCharCode(evt);
+			// var code = eventUtility.getCharCode(evt);
+			var code = evt.keyCode,
+				ctrlKey = evt.ctrlKey;
+			// altKey = evt.altKey,
+			// shiftKey = evt.shiftKey;
 
-			if ((code >= 65 && code <= 90) ||
-				(code >= 97 && code <= 122)) {
-
-			} else {
-				eventUtility.preventDefault(evt);
+			if (ctrlKey && code === 66) {
+				alert("You pressed ctrl+B")
 			}
+
+			// if ((code >= 65 && code <= 90) ||
+			// 	(code >= 97 && code <= 122)) {
+
+			// } else {
+			// 	eventUtility.preventDefault(evt);
+			// }
 
 			// alert(code);
 
