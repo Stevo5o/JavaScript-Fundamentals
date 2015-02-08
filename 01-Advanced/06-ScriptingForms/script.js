ce5938ad67fe07c3e4f92eb105ccc9cc2b4676b0
Stephen <Stephen@O-Connor-ToshL>
2015-02-08 15:00:17 +0000

    Select options selectedOptions remove add

--- 01-Advanced/06-ScriptingForms/script.js
+++ 01-Advanced/06-ScriptingForms/script.js
@@ -12,7 +12,8 @@
 	var form = document.getElementById("theForm");
 		button = form.myButton,
 		textbox = form.myTextbox,
-		textarea = form.myTextArea;
+		textarea = form.myTextArea,
+		select = form.dayOfWeek;
 
 	eventUtility.addEvent(form, "submit", function(evt) {
 		eventUtility.preventDefault(evt);
@@ -21,11 +22,29 @@
 	eventUtility.addEvent(button, "click", function(evt) {
 		var target = eventUtility.getTarget(evt);
 
-		textarea.disabled = true;
+		// // traditional way
+		// var index = select.selectedIndex; // 3
+		// var option = select.options[index];
+
+		// alert(option.value);
+
+		// new way
+		var option = select.selectedOptions[0];
+		alert(option.value);
+
+		select.remove(3);
+
+		var wedOption = new Option("Wednesday", 3);
+
+		select.add(wedOption, select.options[3]);
+
+
+
+		// textarea.disabled = true;
 
 		// textarea.select();
 
-		textarea.value = "asdf";
+		// textarea.value = "asdf";
 
 		// if(textbox.value === "") {
 		// 	alert("please input data in box");
