58bb2144ca0c3567293415769e951975217c79a4
Stevo5o <stevo.joc@gmail.com>
2015-03-12 19:21:08 +0000

    Format

--- 01-Advanced/08-JSON/script.js
+++ 01-Advanced/08-JSON/script.js
@@ -1,6 +1,6 @@
 /**
  * script.js
- * @ Stephen O'Connor, Febuary 2015
+ * @ Stephen O'Connor, March 2015
  *
  * Dependencies:
  * jQuery
@@ -9,34 +9,27 @@
 
 // jQuery AJAX call
 $.ajax({
-			url: 'data.json',
-			dataType: 'json',
-			type: 'get',
-			cache: false,
-			success: function(data) {
-				  // display links on page
-         var output = "";
-         $.each( data, function ( key, val )
-         {
-            for ( var i = 0; i < val.length; i++ )
-            {
-               var user = val[i];
-               output +=
-                       "<li><a id='" +
-                       user.id +
-                       "' href='#" +
-                       user.id + 
-                       "'>" +
-                       "&Tilde;" +
-                       user.username +
-                       "</a></li>";
-            }
-            $( 'ul#users' ).append( output );
-         } );
-      }
-   } );
-			
-	
-                
- 
-                
+	url: 'data.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function(data) {
+		// display links on page
+		var output = "";
+		$.each(data, function(key, val) {
+			for (var i = 0; i < val.length; i++) {
+				var user = val[i];
+				output +=
+					"<li><a id='" +
+					user.id +
+					"' href='#" +
+					user.id +
+					"'>" +
+					"&Tilde;" +
+					user.username +
+					"</a></li>";
+			}
+			$('ul#users').append(output);
+		});
+	}
+});
