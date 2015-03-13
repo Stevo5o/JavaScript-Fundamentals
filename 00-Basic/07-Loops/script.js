be41b3538123e66ee6ffb1b2698539e245d832c7
Stevo5o <stevo.joc@gmail.com>
2015-03-13 11:15:56 +0000

    Eloquent JavaScript

--- 00-Basic/07-Loops/script.js
+++ 00-Basic/07-Loops/script.js
@@ -8,34 +8,34 @@
 //} while (names.length > 0);
 
 /* while (names.length > 0) {
-	var element = names.pop();
-	alert(element);
-} */
+ var element = names.pop();
+ alert(element);
+ } */
 
 /* for (i = 0, len = noNames.length; i < len; i++) {
-	var name = noNames[i];
-	alert(name);
-} // if the condition is false for the first iteration it will not run */
+ var name = noNames[i];
+ alert(name);
+ } // if the condition is false for the first iteration it will not run */
 
 
 /* for (i = 0, len = names.length; i < len; i++) {
-	var name = names[i];
-	alert(name);
-} */
+ var name = names[i];
+ alert(name);
+ } */
 
 /* for (var i = 0; i < names.length; i = i + 1) {
-	var name = names[i];
-	alert(name);
-}
+ var name = names[i];
+ alert(name);
+ }
  */
 /* for (var i = 0; i < names.length; i = i + 1) {
-	var name = names[i];
-	alert(name);
-}
+ var name = names[i];
+ alert(name);
+ }
  */
 /* for (var i = 0; i < 10; i = i + 1) {
-	alert(i);
-} */
+ alert(i);
+ } */
 
 //for(var i =0; i <= 10; i++) {
 //   document.getElementById("forloop").innerHTML += i + "<br>";
@@ -44,8 +44,9 @@
 //console.log("Hello");
 
 
-var	total = 0, count = 1;
-while (count <= 10) {	
+var total = 0,
+	count = 1;
+while (count <= 10) {
 	total += count;
 	count++;
 }
